e17b3d605ef4e2562abfd360484bcf402593c3bd
B_Yoda04 <gerardmatos1990@gmail.com>
2022-04-09 03:25:08 -0500

    style home

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 let toggle = document.getElementById('toggle')
 let menu = document.getElementById('menu')
+let typeOne = document.getElementById('one')
+let typeTwo = document.getElementById('two')
 
 toggle.onclick = function() {
     menu.classList.toggle('active')
 }
+
+setTimeout(() => {
+    
+    typeOne.classList.remove('init')
+    typeTwo.innerText = 'WEB DEVELOPER'
+    typeTwo.classList.add('init')
+    
+    setTimeout(() => {
+        typeTwo.classList.remove('init')
+    }, 2000);
+
+}, 2000);
