6fe6fbd039f281c6358e146d4df7f31c8c65fbc8
B_Yoda04 <gerardmatos1990@gmail.com>
2022-04-10 00:11:18 -0500

    text home computer and change menu style

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
+
+let container = document.getElementById('container')
 let toggle = document.getElementById('toggle')
 let menu = document.getElementById('menu')
 let typeOne = document.getElementById('one')
 let typeTwo = document.getElementById('two')
+let typeTrhee = document.getElementById('trhee')
+let typeFour = document.getElementById('four')
 
 toggle.onclick = function() {
     menu.classList.toggle('active')
+    container.classList.toggle('active')
 }
 
 setTimeout(() => {
-    
-    typeOne.classList.remove('init')
-    typeTwo.innerText = 'WEB DEVELOPER'
-    typeTwo.classList.add('init')
-    
-    setTimeout(() => {
-        typeTwo.classList.remove('init')
-    }, 2000);
 
-}, 2000);
+    typeOne.innerText = `HI I'm Javier Matos`
+    typeOne.classList.add('init')
+
+    setTimeout(() => {
+    
+        typeOne.classList.remove('init')
+    
+        //span two
+        typeTwo.innerText = 'UX/UI DESIGNER'
+        typeTwo.classList.add('init')
+        
+        setTimeout(() => {
+            typeTwo.classList.remove('init')
+    
+            //span three
+            typeTrhee.innerText = 'FRONTEND DEVELOPER'
+            typeTrhee.classList.add('init')
+            
+            setTimeout(() => {
+                typeTrhee.classList.remove('init')
+    
+                //span four
+                typeFour.innerText = 'BACKEND DEVELOPER'
+                typeFour.classList.add('init')
+                
+                setTimeout(() => {
+                    typeFour.classList.remove('init')
+                }, 2000);
+            }, 2000);
+        }, 2000);
+    
+    }, 2000);
+    
+}, 500);
+
